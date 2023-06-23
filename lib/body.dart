5db9b49cc8687d7f4e02e5de8b4f0bd396e0fe93
Deannah_Beb <deannahbeb@gmail.com>
2023-06-23 07:59:33 +0000

    added images and text

--- lib/body.dart
+++ lib/body.dart
@@ -3,8 +3,22 @@ import 'package:flutter/material.dart';
 class Body extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: CircularProgressIndicator(),
+    return ListView.builder(
+      itemCount: 50,
+      itemBuilder: (context, rowNumber) {
+        return Column(
+          children: [
+            Image.asset("assets/cocis.jpg"),
+            Divider(
+              color: Colors.blue,
+            ),
+            Text("Welcome to CoCIS", style: TextStyle(fontSize: 16.0)),
+            Divider(
+              color: Colors.blue,
+            ),
+          ],
+        );
+      },
     );
   }
 }
