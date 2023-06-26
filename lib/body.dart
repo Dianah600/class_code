f49dd5b7f060472832de5ca81a9bee6f156731a3
Deannah_Beb <deannahbeb@gmail.com>
2023-06-26 07:26:35 +0000

    added assets

--- lib/body.dart
+++ lib/body.dart
@@ -6,17 +6,20 @@ class Body extends StatelessWidget {
     return ListView.builder(
       itemCount: 50,
       itemBuilder: (context, rowNumber) {
-        return Column(
-          children: [
-            Image.asset("assets/cocis.jpg"),
-            Divider(
-              color: Colors.blue,
-            ),
-            Text("Welcome to CoCIS", style: TextStyle(fontSize: 16.0)),
-            Divider(
-              color: Colors.blue,
-            ),
-          ],
+        return Container(
+          padding: EdgeInsets.all(15.0),
+          child: Column(
+            children: [
+              Image.asset("assets/cocis2.jpg"),
+              Divider(
+                color: Colors.blue,
+              ),
+              Text("Welcome to CoCIS", style: TextStyle(fontSize: 16.0)),
+              Divider(
+                color: Colors.blue,
+              ),
+            ],
+          ),
         );
       },
     );
